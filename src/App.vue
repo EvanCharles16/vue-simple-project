7c0fd87abc860693a89def8934834c811817ff24
EvanCharles16 <evancharles1609@gmail.com>
2020-08-09 15:29:22 +0700

    add progress bar quote

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="container">
+    <!-- Passing quoteCount property -->
+    <app-header class="mt-5" :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
     <div class="row">
       <div class="col-md-12">
         <!-- Listen to event quoteAdded from selector's NewQuote.vue -->
@@ -22,16 +24,21 @@
 <script>
 import QuoteGrid from "./components/QuoteGrid";
 import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
 
 export default {
   data: function () {
     return {
       quotes: ["Just a Quote to see something"],
       maxQuotes: 10,
+      quoteCount: 0,
     };
   },
   methods: {
     newQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Please delete Quotes first");
+      }
       this.quotes.push(quote);
     },
     deleteQuote(index) {
@@ -41,6 +48,7 @@ export default {
   components: {
     appQuoteGrid: QuoteGrid,
     appNewQuote: NewQuote,
+    appHeader: Header,
   },
 };
 </script>
